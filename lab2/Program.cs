87cdda9143263faf4bedc3964ecc213a29ea8ac2
vit303 <vitalyzaozernov@gmail.com>
2024-11-01 01:32:36 +0700

    Добавьте файлы проекта.

---
+++ lab2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Vector v = new Vector(23);
+            v.AddValue(24);
+            Vector v1 = new Vector(-25);
+            v1.AddValue(26);
+            Vector result = v + v1;
+            Vector r = new Vector();
+            if(r == result);
+            Console.WriteLine(string.Join(", ", r.ReturnValue()));
+            r =r* 2;
+            Console.WriteLine(string.Join(", ", r.ReturnValue()));
+            r = Vector.Delete(r);
+            Console.WriteLine(string.Join(", ", r.ReturnValue()));
+        }
+    }
+}
---
+++ lab2/Vector.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    internal class Vector
+    {
+        protected List<double> vector = new List<double>();
+
+        public Vector(double value) {
+            this.vector.Add(value);
+        }
+
+        public Vector() {
+            this.vector = new List<double>();
+        }
+
+        public void AddValue(double value)
+        {
+            this.vector.Add(value);
+        }
+
+        public List<double> ReturnValue() { 
+            return this.vector;
+        }
+
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            Vector result = new Vector(v1.vector[0]); 
+            result.vector.AddRange(v1.vector.GetRange(1, v1.vector.Count - 1)); 
+            result.vector.AddRange(v2.vector); 
+            return result; 
+        }
+
+        public static bool operator >(Vector v1, Vector v2) {
+            return v1.vector.Count > v2.vector.Count;
+        }
+
+        public static bool operator <(Vector v1, Vector v2)
+        {
+            return v1.vector.Count < v2.vector.Count;
+        }
+
+        public static bool operator ==(Vector v1, Vector v2)
+        {
+            foreach (double value in v2.vector)
+            {
+                v1.AddValue(value);
+            }
+            return true;
+        }
+
+        public static bool operator !=(Vector v1, Vector v2)
+        {
+            if (v1.vector.Count > v2.vector.Count || v1.vector.Count < v2.vector.Count) { 
+                return false;
+            }
+            for (int i = 0; i < v1.vector.Count; i++) {
+                if (v1.vector[i] != v2.vector[i])
+                {
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
+            return true;
+        }
+
+        public static Vector operator *(Vector v, double n) {
+            for (int i = 0; i < v.vector.Count; i++)
+            {
+                v.vector[i] = ((double)v.vector[i] * n);
+            }
+            return v;
+        }
+
+        public static Vector Delete(Vector v) {
+            for (int i = 0; i < v.vector.Count; i++)
+            {
+                if (v.vector[i] < 0){
+                    v.vector.RemoveAt(i);
+                }
+            }
+            return v;
+        }
+
+    }
+}
